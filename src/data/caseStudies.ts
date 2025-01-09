4125d141102d6d617c417b8fc09102409688d1d0
karina <hllo@karinatovar.com>
2025-01-09 03:29:14 -0500

    beginning of work page

---
+++ src/data/caseStudies.ts
@@ -0,0 +1,44 @@
+import tropicImage from "../assets/blog/ruby-on-rails.png";
+
+export interface CaseStudy {
+  title: string;
+  subtitle: string;
+  description: string;
+  tags: string[];
+  image: ImageMetadata;
+  imageAlt: string;
+  href: string;
+}
+
+export const caseStudies: CaseStudy[] = [
+  {
+    title: "Tropic",
+    subtitle: "Building Supplier Intelligence",
+    description:
+      "I am an artist, turned product designer, who likes to code and make things. That just means that I am obsessed over how",
+    tags: ["Product Design", "Strategy", "Design Engineering"],
+    image: tropicImage,
+    imageAlt: "Tropic platform interface showing supplier management features",
+    href: "/work/tropic",
+  },
+  {
+    title: "Tropic",
+    subtitle: "Building Supplier Intelligence",
+    description:
+      "I am an artist, turned product designer, who likes to code and make things. That just means that I am obsessed over how",
+    tags: ["Product Design", "Strategy", "Design Engineering"],
+    image: tropicImage,
+    imageAlt: "Tropic platform interface showing supplier management features",
+    href: "/work/tropic",
+  },
+  {
+    title: "Tropic",
+    subtitle: "Building Supplier Intelligence",
+    description:
+      "I am an artist, turned product designer, who likes to code and make things. That just means that I am obsessed over how",
+    tags: ["Product Design", "Strategy", "Design Engineering"],
+    image: tropicImage,
+    imageAlt: "Tropic platform interface showing supplier management features",
+    href: "/work/tropic",
+  },
+]; 
