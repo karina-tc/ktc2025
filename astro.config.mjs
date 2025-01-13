e901be5cb4cd35b2924a1afeb669cdc9e670e098
karina <hllo@karinatovar.com>
2025-01-13 00:10:15 -0500

    resume page update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@
 import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
+import yaml from '@rollup/plugin-yaml';
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [mdx()],
     vite: {
+        plugins: [yaml()],
         css: {
             preprocessorOptions: {
                 scss: {
--- src/data/resume.ts
+++ src/data/resume.ts
@@ -1,5 +1,7 @@
 import type { ImageMetadata } from 'astro';
+import workExperienceData from './resume.yaml';
 
+// Import images
 import renumerate from "../assets/icons/cv-renumerate.png";
 import tropic from "../assets/icons/cv-tropic.png";
 import mystery from "../assets/icons/cv-glue.png";
@@ -11,12 +13,17 @@ import mit from "../assets/icons/cv-mit.png";
 import massart from "../assets/icons/cv-massart.png";
 import utep from "../assets/icons/cv-utep.png";
 
+interface Achievement {
+  text: string;
+}
+
 interface Company {
-  logo: ImageMetadata;
+  logo: keyof typeof logoMap;
   name: string;
   role: string;
   period: string;
-  description: string;
+  intro: string;
+  achievements: string[];
   companyUrl?: string;
 }
 
@@ -37,96 +44,27 @@ interface Education {
   degree: string;
 }
 
-export const workExperience: YearSection[] = [
-  {
-    year: "Now",
-    companies: [
-      {
-        logo: renumerate,
-        name: "Renumerate",
-        role: "Fractional Product Designer",
-        period: "2024-Present",
-        description: "Helped out with the branding and kicked off the product strategy and design.",
-      },
-    ],
-  },
-  {
-    year: "2024",
-    companies: [
-      {
-        logo: tropic,
-        name: "Tropic",
-        role: "Lead Product Designer",
-        period: "2024-Present",
-        description: "My motivation is to build products that aim to help people have happier days. Life is too short to be ruined by awful experiences. Every product needs to be crafted for human joy.",
-        companyUrl: "https://www.tropicapp.io",
-      },
-      {
-        logo: tropic,
-        name: "Tropic",
-        role: "Senior Product Designer",
-        period: "2023-2024",
-        description: "My motivation is to build products that aim to help people have happier days. Life is too short to be ruined by awful experiences. Every product needs to be crafted for human joy.",
-        companyUrl: "https://www.tropicapp.io",
-      },
-    ],
-  },
-  {
-    year: "2023",
-    companies: [
-      {
-        logo: mystery,
-        name: "Mystery / Glue",
-        role: "Senior Product Designer",
-        period: "2022-2023",
-        description: "My motivation is to build products that aim to help people have happier days. Life is too short to be ruined by awful experiences. Every product needs to be crafted for human joy.",
-        companyUrl: "https://www.glue.co",
-      },
-    ],
-  },
-  {
-    year: "2022",
-    companies: [
-      {
-        logo: teikametrics,
-        name: "Teikametrics",
-        role: "Staff Product Designer",
-        period: "2021-2022",
-        description: "My motivation is to build products that aim to help people have happier days. Life is too short to be ruined by awful experiences. Every product needs to be crafted for human joy.",
-        companyUrl: "https://www.teikametrics.com",
-      },
-      {
-        logo: teikametrics,
-        name: "Teikametrics",
-        role: "Senior Product Designer",
-        period: "2020-2021",
-        description: "My motivation is to build products that aim to help people have happier days. Life is too short to be ruined by awful experiences. Every product needs to be crafted for human joy.",
-        companyUrl: "https://www.teikametrics.com",
-      },
-    ],
-  },
-  {
-    year: "2020",
-    companies: [
-      {
-        logo: rejoiner,
-        name: "Rejoiner",
-        role: "Product Owner",
-        period: "2019-2020",
-        description: "My motivation is to build products that aim to help people have happier days. Life is too short to be ruined by awful experiences. Every product needs to be crafted for human joy.",
-        companyUrl: "https://www.rejoiner.com",
-      },
-      {
-        logo: rejoiner,
-        name: "Rejoiner",
-        role: "Design Intern",
-        period: "2014-2020",
-        description: "My motivation is to build products that aim to help people have happier days. Life is too short to be ruined by awful experiences. Every product needs to be crafted for human joy.",
-        companyUrl: "https://www.rejoiner.com",
-      },
-    ],
-  },
-];
+// Complete logo map with all images
+const logoMap = {
+  renumerate,
+  tropic,
+  mystery,
+  teikametrics,
+  rejoiner,
+  venly,
+  mit,
+  massart,
+  utep
+};
+
+// Process the YAML data to add the actual image imports
+export const workExperience: YearSection[] = workExperienceData.workExperience.map((section: YearSection) => ({
+  ...section,
+  companies: section.companies.map((company: Company) => ({
+    ...company,
+    logo: logoMap[company.logo as keyof typeof logoMap] || '',  // Add fallback
+  }))
+}));
 
 export const otherExperience: OtherExperience[] = [
   {
