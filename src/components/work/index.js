9c951a30d8a1a5872521964e8602d2c369b338fe
karina <hllo@karinatovar.com>
2025-01-29 16:12:57 -0500

    fine tunings

---
+++ src/components/work/index.js
@@ -0,0 +1,7 @@
+export { default as CaseStudyHeader } from "./CaseStudyHeader.astro";
+export { default as CaseStudyShowcase } from "./CaseStudyShowcase.astro";
+export { default as ImpactSection } from "./ImpactSection.astro";
+export { default as ScrollableGallery } from "./ScrollableGallery.astro";
+export { default as StorySection } from "./StorySection.astro";
+export { default as RocketShipPlate } from "./RocketShipPlate.astro";
+export { default as GridFeatures } from "./GridFeatures.astro"; 
