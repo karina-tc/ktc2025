28597e61672d7a19a992b6feb88f71b2ae419822
karina <hllo@karinatovar.com>
2025-01-29 16:24:40 -0500

    footer add

--- src/components/work/index.js
+++ src/components/work/index.js
@@ -4,4 +4,5 @@ export { default as ImpactSection } from "./ImpactSection.astro";
 export { default as ScrollableGallery } from "./ScrollableGallery.astro";
 export { default as StorySection } from "./StorySection.astro";
 export { default as RocketShipPlate } from "./RocketShipPlate.astro";
-export { default as GridFeatures } from "./GridFeatures.astro"; 
+export { default as GridFeatures } from "./GridFeatures.astro";
+export { default as WorkFooter } from "./WorkFooter.astro";
